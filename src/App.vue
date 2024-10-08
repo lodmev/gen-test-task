39c2e384c365b7fdd2ae82b8b92d4b82e0c67dcb
lodmev <lodmev@gmail.com>
2024-10-08 12:31:27 +0300

    feat: create app, add components templates

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ActionsDropdown from './components/ActionsDropdown.vue'
+import CreateButton from './components/CreateButton.vue'
+import CreatedEntities from './components/CreatedEntities.vue'
+</script>
+
+<template>
+  <header><h1>Simple Amo Plugin</h1></header>
+  <main class="container">
+    <actions-dropdown></actions-dropdown>
+    <create-button></create-button>
+    <created-entities></created-entities>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  text-align: center;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/CreateButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button>Создать</button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
